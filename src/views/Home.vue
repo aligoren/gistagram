f3359e5ebedef86d6edb0c608d2fe64c6313f42a
Ali GOREN <goren.ali@yandex.com>
2020-09-04 15:56:21 +0300

    First commit

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <navbar />
+        <feed />
+    </div>
+</template>
+
+<script>
+// https://github.com/codingvenue/instagram-hompage-clone-bootstrap
+export default {
+    name: 'Home',
+    components: {
+        'navbar': () => import("./Navbar"),
+        'feed': () => import("./Feed")
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
